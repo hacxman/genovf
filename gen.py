991b81a7943ae196298f49c4dddb1637a3c9f13d
Maros Zatko <mzatko@redhat.com>
2013-05-23 16:08:24 +0200

    raw->vmdk convert, gen XML fragments for disks

--- gen.py
+++ gen.py
@@ -1,41 +1,154 @@
+#!/usr/bin/python
+
 from string import Template
 import sys
 import os
+from VMDKstream import convert_to_stream
+
 
 tpltypes = {'vsphere': 'vsphere.xml.tpl'}
 
-def doit(tplname, outname):
+reftpl = '<File ovf:href="$vmdkname" ovf:id="$fileid" ovf:size="$filemaxsize"/>'
+disktpl = '<Disk ovf:capacity="$capacity" ovf:capacityAllocationUnits="byte" ovf:diskId="$diskid" ovf:fileRef="$fileid" ovf:format="http://www.vmware.com/interfaces/specifications/vmdk.html#streamOptimized" ovf:populatedSize="$populsize"/>'
+diskitemtpl = '''
+      <Item>
+        <rasd:AddressOnParent>$idx</rasd:AddressOnParent>
+        <rasd:ElementName>Hard disk $idx</rasd:ElementName>
+        <rasd:HostResource>ovf:/disk/$diskid</rasd:HostResource>
+        <rasd:InstanceID>$instanceid</rasd:InstanceID>
+        <rasd:Parent>3</rasd:Parent>
+        <rasd:ResourceType>17</rasd:ResourceType>
+        <vmw:Config ovf:required="false" vmw:key="backing.writeThrough" vmw:value="false"/>
+      </Item>'''
+
+def generate_manifest():
+  fout = NamedTemporaryFile(delete=False)
+  # hashlib is stupid so we can't use map :(
+  mf = self.generate_manifest_data() 
+  fout.write(mf)
+  self.log.debug("NAME %s" % fout.name)
+  os.rename(fout.name, '/tmp/manifest.mf')
+
+def generate_manifest_data():
+  shas = []
+  mf = ''
+  for vols in self.images.keys():
+    for img in self.images[vols]:
+      with open(img, 'rb') as fin:
+        digest = hashlib.sha256(fin.read()).hexdigest()
+        mf += "SHA256(%s)= %s\n" % (img, digest)
+
+  return mf
+
+
+def convert_images(files, outpath):
+  ofils = []
+  for f in files:
+    foutname = os.path.join(outpath, os.path.basename(f)+'.vmdk')
+    sys.stdout.write('Converting {0} to {1}\n'.format(f, foutname))
+    convert_to_stream(f, foutname)
+
+    ofils.append(foutname)
+
+  sys.stdout.write('Converting done.\n')
+  return ofils
+
+def construct_refs(intpl, filenames):
+  idx = 1
+  orefs = [] # output File Referencies fragments for OVF
+  for fin in filenames:
+    size = os.stat(fin).st_size
+    sparsesize = os.stat(fin).st_blocks*512
+
+    d = {'vmdkname': fin, 'fileid': 'file'+str(idx), 'maxfilesize': str(size)}
+    tpl = Template(intpl)
+    orefs.append(tpl.substitute(d))
+
+  return orefs
+
+def construct_disks(intpl, filenames):
+  idx = 1
+  orefs = [] # output Disk Section fragments for OVF
+  for fin in filenames:
+    size = os.stat(fin).st_size
+    sparsesize = os.stat(fin).st_blocks*512
+
+    d = {'capacity': str(size), 'diskid': 'vmdisk'+str(idx),
+         'fileid': 'file'+str(idx), 'populsize': str(sparsesize)}
+    tpl = Template(intpl)
+    orefs.append(tpl.substitute(d))
+
+  return orefs
+
+def construct_hw_disks(intpl, diskids):
+  idx = 1
+  orefs = [] # output Disk Section fragments for OVF
+  for did in diskids:
+    d = {'idx': idx, 'diskid': did, 'instanceid': 17+idx}
+    #TODO: this instanceid thingy needs systematic fix
+    #      17 is magical number
+
+    tpl = Template(intpl)
+    orefs.append(tpl.substitute(d))
+
+  return orefs
+
+
+
+def doit(tplname, outname, inputimages):
   with open(tplname) as ftpl:
     tpl = Template(ftpl.read())
     d = {'cpucount': 10000}
+
+    refs = construct_refs(reftpl, inputimages)
+    disks = construct_disks(disktpl, inputimages)
+    hwdisks = construct_hw_disks(diskitemtpl, disks)
+
+    print refs, disks, hwdisks
 
     outtpl = tpl.substitute(d)
 
     with open(outname, 'w+') as ofil:
       ofil.write(outtpl)
 
-    os.stdout.write('{0} wrote.'.format(outname))
+    sys.stdout.write('{0} wrote.\n'.format(outname))
 
 def showusage():
-  os.stderr.write('''usage: ./gen.py [-t TYPE] [-o OUTFILE] [-h]
+  sys.stderr.write('''usage: ./gen.py [-t TYPE] [-o OUTFILE] [-c] [-h] -i IMAGE1 IMAGE2...
       where
         -t TYPE is one of: vsphere(default)
         -o OUTFILE is output OVF name (default output.ovf)
-        -h shows this help
-      ''')
+        -c convert raw imgs to streamable VMDKs
+        -i specifies list of input disk imgs
+        -h shows this help\n''')
 
 if __name__ == '__main__':
   typ = 'vsphere'
   outfile = 'output.ovf'
+  cvt = False
+  inputimages = []
 
   if '-t' in sys.argv:
     typ = sys.argv[sys.argv.index('-t')+1]
     if typ not in tpltypes.keys():
-      os.stderr.write('{0} is unsupported, supported -t options are {1}'.format(typ, tpltypes.keys()))
+      sys.stderr.write('{0} is unsupported, supported -t options are {1}\n'.format(typ, tpltypes.keys()))
   if '-o' in sys.argv:
     outfile = sys.argv[sys.argv.index('-o')+1]
+  if '-c' in sys.argv:
+    cvt = True
   if '-h' in sys.argv:
     showusage()
     exit(1)
+  if '-i' in sys.argv:
+    argidx = sys.argv.index('-i')+1
+    for inf in sys.argv[argidx:]:
+      if not inf.startswith('-'): 
+        inputimages.append(inf)
+  else:
+    sys.stderr.write('-i argument is mandatory. exitting.\n')
+    exit(2)
 
-  doit(tpltypes[typ], outfile)
+  if cvt:
+    inputimages = convert_images(inputimages, '.')
+
+  doit(tpltypes[typ], outfile, inputimages)
